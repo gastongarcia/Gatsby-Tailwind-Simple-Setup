b6bc1ad7c2b57ccfd684d0c5ad2e0c63981fcbf3
gastongarcia <gaston.garcia@gmail.com>
2019-06-25 11:53:08 -0600

    Setting links to Tailwind, Gatsby and Github repo

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,12 @@ const IndexPage = () => (
 
     <div className="md:w-1/2 w-10/12 mx-auto bg-gray-300 mt-5 p-3 rounded-lg">
       <p className="text-lg my-5">Gatsby &amp; Tailwind</p>
-      <p className="text-base mb-5">This is a very simple setup of Tailwind in a Gatsby project. The only plugins it uses are <a href="https://www.gatsbyjs.org/packages/gatsby-plugin-postcss/?=post" target="_blank" className="underline">Gatsby Post CSS</a> and <a href="https://www.gatsbyjs.org/packages/gatsby-plugin-purgecss/?=purge" target="_blank" className="underline">Gatsby Purge CSS</a>.</p>
+      <p className="text-base mb-5">This is a very simple setup of <a href="https://tailwindcss.com/" target="_blank" className="underline">Tailwind</a> in a <a href="https://www.gatsbyjs.org" target="_blank" className="underline">Gatsby</a> project. The only plugins it uses are <a href="https://www.gatsbyjs.org/packages/gatsby-plugin-postcss/?=post" target="_blank" className="underline">Gatsby Post CSS</a> and <a href="https://www.gatsbyjs.org/packages/gatsby-plugin-purgecss/?=purge" target="_blank" className="underline">Gatsby Purge CSS</a>.</p>
     </div>
 
     <div className="md:w-1/2 w-10/12 mx-auto bg-gray-200 mt-5 p-3 rounded-lg">
       <p className="text-lg my-5">Setup</p>
-      <p className="text-base mb-5">You can download or clone the source from it's <a href="" target="_blank" className="underline">Github repo here</a>. Hope it helps.</p>
+      <p className="text-base mb-5">You can download or clone the source from it's <a href="https://github.com/gastongarcia/Gatsby-Tailwind-Simple-Setup" target="_blank" className="underline">Github repo here</a>. Hope it helps.</p>
     </div>
 
   </Layout>
