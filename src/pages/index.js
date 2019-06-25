a19d207e2be8bca8076443fd6bd2b4e247d0a86a
gastongarcia <gaston.garcia@gmail.com>
2019-06-25 11:47:46 -0600

    Setting width for medium and large screens

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,12 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Gatsby Tailwind Simple Setup" />
 
-    <div className="md-w-1/2 w-10/12 mx-auto bg-gray-300 mt-5 p-3 rounded-lg">
+    <div className="md:w-1/2 w-10/12 mx-auto bg-gray-300 mt-5 p-3 rounded-lg">
       <p className="text-lg my-5">Gatsby &amp; Tailwind</p>
       <p className="text-base mb-5">This is a very simple setup of Tailwind in a Gatsby project. The only plugins it uses are <a href="https://www.gatsbyjs.org/packages/gatsby-plugin-postcss/?=post" target="_blank" className="underline">Gatsby Post CSS</a> and <a href="https://www.gatsbyjs.org/packages/gatsby-plugin-purgecss/?=purge" target="_blank" className="underline">Gatsby Purge CSS</a>.</p>
     </div>
 
-    <div className="md-w-1/2 w-10/12 mx-auto bg-gray-200 mt-5 p-3 rounded-lg">
+    <div className="md:w-1/2 w-10/12 mx-auto bg-gray-200 mt-5 p-3 rounded-lg">
       <p className="text-lg my-5">Setup</p>
       <p className="text-base mb-5">You can download or clone the source from it's <a href="" target="_blank" className="underline">Github repo here</a>. Hope it helps.</p>
     </div>
